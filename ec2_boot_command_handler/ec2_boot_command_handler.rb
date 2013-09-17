cec6996e1ccd6e736dae8207a6b57b85d09dbabe
Kieran Manning <kieran.manning@wonga.com>
2013-09-17 18:04:31 +0100

    fixing typos, adding tests

--- ec2_boot_command_handler/ec2_boot_command_handler.rb
+++ ec2_boot_command_handler/ec2_boot_command_handler.rb
@@ -52,13 +52,13 @@ module Wonga
         @publisher.publish(message_out)
       end
 
-      def boot_machine(request_id, instance_name, domain, flavor, ami, team_id, subnet_id, secgroup_ids, key_name, user_data)
-        instance = create_instance(ami, flavor, secgroup_ids, subnet_id, key_name, user_data)
+      def boot_machine(request_id, instance_name, domain, flavor, ami, team_id, subnet_id, secgroup_ids, key_name, block_device_hash, user_data)
+        instance = create_instance(ami, flavor, secgroup_ids, subnet_id, key_name, block_device_hash, user_data)
         tag_and_wait_instance(instance, request_id, instance_name, domain, team_id)
       end
 
-      def create_instance(ami, flavor, secgroup_ids, subnet_id, key_name, vol_size, user_data)
-        instance = @ec2.instances.create(
+      def create_instance(ami, flavor, secgroup_ids, subnet_id, key_name, block_device_hash, user_data)
+        @ec2.instances.create(
           image_id:               ami,
           instance_type:          flavor,
           count:                  1,
@@ -67,7 +67,7 @@ module Wonga
           key_name:               key_name,
           user_data:              user_data,
           block_device_mappings:  block_device_hash
-        return instance
+        )
       end
 
       def find_machine_by_request_id(request_id)
